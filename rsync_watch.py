e113488a259b9f5a537394dcb24133f6ba7aecec
Josef Friedrich <josef@friedrich.rocks>
2019-03-17 11:21:36 +0100

    Parse stats

---
+++ rsync_watch.py
@@ -0,0 +1,153 @@
+#! /usr/bin/env python3
+
+import argparse
+import subprocess
+import re
+
+
+
+class RsyncWatchError(Exception):
+    """Base exception for this script."""
+
+
+class StatsNotFoundError(RsyncWatchError):
+    """Raised when some stats regex couldn’t be found in stdout."""
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='A Python script to monitor the execution of a rsync task.'
+    )
+    parser.add_argument(
+        'src',
+        help='The source ([[USER@]HOST:]SRC)'
+    )
+    parser.add_argument(
+        'dest',
+        help='The destination ([[USER@]HOST:]DEST)')
+
+    return parser.parse_args()
+
+
+def parse_stats(stdout):
+    """Parse the standard output of the rsync process.
+
+    :param string stdout: The standard output of the rsync process
+
+    :return: A dictionary containing all the stats numbers.
+    :rtype: dict
+    """
+    result = {}
+
+    # num_files
+    # num_dirs
+    match = re.search(r'\nNumber of files: (\d*) \(dir: (\d*)\)\n', stdout)
+    if match:
+        result['num_files'] = int(match[1])
+        result['num_dirs'] = int(match[2])
+    else:
+        raise StatsNotFoundError('Number of files: X (dir: X)')
+
+    # num_created_files
+    match = re.search(r'\nNumber of created files: (\d*)\n', stdout)
+    if match:
+        result['num_created_files'] = int(match[1])
+    else:
+        raise StatsNotFoundError('Number of created files: X')
+
+    # num_deleted_files
+    match = re.search(r'\nNumber of deleted files: (\d*)\n', stdout)
+    if match:
+        result['num_deleted_files'] = int(match[1])
+    else:
+        raise StatsNotFoundError('Number of deleted files: X')
+
+    # num_files_transferred
+    match = re.search(r'\nNumber of regular files transferred: (\d*)\n',
+                      stdout)
+    if match:
+        result['num_files_transferred'] = int(match[1])
+    else:
+        raise StatsNotFoundError('Number of regular files transferred: X')
+
+    # total_size
+    match = re.search(r'\nTotal file size: (\d*) bytes\n', stdout)
+    if match:
+        result['total_size'] = int(match[1])
+    else:
+        raise StatsNotFoundError('Total file size: X bytes')
+
+    # transferred_size
+    match = re.search(r'\nTotal transferred file size: (\d*) bytes\n', stdout)
+    if match:
+        result['transferred_size'] = int(match[1])
+    else:
+        raise StatsNotFoundError('Total transferred file size: X bytes')
+
+    # literal_data
+    match = re.search(r'\nLiteral data: (\d*) bytes\n', stdout)
+    if match:
+        result['literal_data'] = int(match[1])
+    else:
+        raise StatsNotFoundError('Literal data: X bytes')
+
+    # matched_data
+    match = re.search(r'\nMatched data: (\d*) bytes\n', stdout)
+    if match:
+        result['matched_data'] = int(match[1])
+    else:
+        raise StatsNotFoundError('Matched data: X bytes')
+
+    # list_size
+    match = re.search(r'\nFile list size: (\d*)\n', stdout)
+    if match:
+        result['list_size'] = int(match[1])
+    else:
+        raise StatsNotFoundError('File list size: X')
+
+    # list_generation_time
+    match = re.search(r'\nFile list generation time: ([\d\.]*) seconds\n',
+                      stdout)
+    if match:
+        result['list_generation_time'] = float(match[1])
+    else:
+        raise StatsNotFoundError('File list generation time: X.XXX seconds')
+
+    # list_transfer_time
+    match = re.search(r'\nFile list transfer time: ([\d\.]*) seconds\n', stdout)
+    if match:
+        result['list_transfer_time'] = float(match[1])
+    else:
+        raise StatsNotFoundError('File list transfer time: X.XXX seconds')
+
+    # bytes_sent
+    match = re.search(r'\nTotal bytes sent: (\d*)\n', stdout)
+    if match:
+        result['bytes_sent'] = int(match[1])
+    else:
+        raise StatsNotFoundError('Total bytes sent: X')
+
+    # bytes_received
+    match = re.search(r'\nTotal bytes received: (\d*)\n', stdout)
+    if match:
+        result['bytes_received'] = int(match[1])
+    else:
+        raise StatsNotFoundError('Total bytes received: X')
+
+    return result
+
+
+def main():
+    args = parse_args()
+
+    process = subprocess.run(
+        ['rsync', '-av', '--stats', args.src, args.dest],
+        encoding='utf-8',
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+    print(process.stdout)
+
+
+if __name__ == "__main__":
+    main()
--- test/test_rsync_watch.py
+++ test/test_rsync_watch.py
@@ -1,10 +1,60 @@
 import unittest
 import subprocess
 import os
+from rsync_watch import parse_stats, StatsNotFoundError
 
 SCRIPT = os.path.realpath(
-    os.path.join(os.path.dirname(__file__), '..', 'rsync-watch.py')
+    os.path.join(os.path.dirname(__file__), '..', 'rsync_watch.py')
 )
+
+OUTPUT1 = '''
+sending incremental file list
+
+Number of files: 1 (dir: 2)
+Number of created files: 3
+Number of deleted files: 4
+Number of regular files transferred: 5
+Total file size: 6 bytes
+Total transferred file size: 7 bytes
+Literal data: 8 bytes
+Matched data: 9 bytes
+File list size: 10
+File list generation time: 11.000 seconds
+File list transfer time: 12.000 seconds
+Total bytes sent: 13
+Total bytes received: 14
+
+sent 61 bytes  received 17 bytes  156.00 bytes/sec
+total size is 0  speedup is 0.00
+'''
+
+
+class TestUnitParseStats(unittest.TestCase):
+
+    def test_empty_string(self):
+        with self.assertRaises(StatsNotFoundError) as exception:
+            parse_stats('')
+        self.assertEqual(str(exception.exception),
+                         'Number of files: X (dir: X)')
+
+    def test_output1(self):
+        result = parse_stats(OUTPUT1)
+        self.assertEqual(result, {
+            'num_files': 1,
+            'num_dirs': 2,
+            'num_created_files': 3,
+            'num_deleted_files': 4,
+            'num_files_transferred': 5,
+            'total_size': 6,
+            'transferred_size': 7,
+            'literal_data': 8,
+            'matched_data': 9,
+            'list_size': 10,
+            'list_generation_time': 11.000,
+            'list_transfer_time': 12.000,
+            'bytes_sent': 13,
+            'bytes_received': 14,
+        })
 
 
 class TestIntegration(unittest.TestCase):
@@ -29,7 +79,7 @@ class TestIntegration(unittest.TestCase):
         )
         self.assertEqual(process.returncode, 0)
         self.assertIn(
-            'usage: rsync-watch.py [-h] src dest',
+            'usage: rsync_watch.py [-h] src dest',
             process.stdout
         )
 
